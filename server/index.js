cad43a785c35616367246c114b1c3c608fab9777
Joao Vitor <joaovitor2207@gmail.com>
2021-07-10 19:50:34 -0300

    Teste

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
 const app = require('./config/express-config');
 
+app.get('/', (rec, res)=>{
+res.send('hello world');
+
+})
+
 app.listen(3000, 'localhost', () => console.log('Servidor Rodando!'))
