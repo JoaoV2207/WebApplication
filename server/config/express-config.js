4e79e4cd74518fb9fb9ea1a6c13cf149d47e99ce
Joao Vitor <joaovitor2207@gmail.com>
2021-07-12 20:15:22 -0300

    Algumas configurações

--- server/config/express-config.js
+++ server/config/express-config.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const app = require('./config/express-config');
 
-app.get('/', (rec, res)=>{
+app.get('/', (req, res)=>{
 res.send('hello world');
 
 })
 
+app.get('/user', (req,res) => {
+const pessoa = {
+    nome: 'Joao',
+    sobrenome: 'Silva'
+}
+res.json(pessoa);
+})
+
+app.post('/mirror', (req, res) => {
+    res.json(req.body)
+})
 app.listen(3000, 'localhost', () => console.log('Servidor Rodando!'))
