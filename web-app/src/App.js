e3e9f44a55a494d2ac70a97a75895a239c5253ab
Joao Vitor <joaovitor2207@gmail.com>
2021-08-19 17:28:03 -0300

    Mudanças

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -9,6 +9,11 @@ import './App.css';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
+import Books from './components/Books/Books';
+import Sedes from './components/Sedes/Sedes'
+import News from './components/News/News'
+import Sede from './components/Sede/Sede'
+
 
 function App() {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL;
@@ -16,6 +21,21 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+        <Route path="/Sede">
+            <Sede/>
+          </Route>
+        <Route path="/Novidades">
+            <News/>
+          </Route>
+        <Route path="/Home">
+            <Home />
+          </Route>
+        <Route path="/Sedes">
+            <Sedes />
+          </Route>
+        <Route path="/Books">
+            <Books />
+          </Route>
         <Route path="/Dashboard">
             <Dashboard />
           </Route>
--- web-app/src/components/Home/Home.jsx
+++ web-app/src/components/Home/Home.jsx
@@ -36,14 +36,17 @@ export default function Home(){
                         <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
                           <ul className="navbar-nav  ">
                             <li className="nav-item active">
-                              <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+                              <a className="nav-link" href="/Home">Home <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
-                              <a className="nav-link" href="Sedes.jsx"> Sedes </a>
+                              <a className="nav-link" href="/Sedes"> Sedes </a>
                             </li>
                             <li className="nav-item">
-                              <a className="nav-link" href="books.html"> Livros </a>
+                              <a className="nav-link" href="/Books"> Livros </a>
                             </li>
+                            <li className="nav-item">
+                          <a className="nav-link" href="/Novidades"> Novidades </a>
+                        </li>
                             <li className="nav-item">
                               <a className="nav-link" href="login.html">Adicionar</a>
                             </li>
