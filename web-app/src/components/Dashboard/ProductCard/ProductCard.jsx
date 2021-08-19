2d13f36b41acf18d805bbfebce8b1e703707413f
Daniel Duarte <danalvesduarte@gmail.com>
2021-08-19 17:58:18 -0300

    Update ProductCard.jsx

--- web-app/src/components/Dashboard/ProductCard/ProductCard.jsx
+++ web-app/src/components/Dashboard/ProductCard/ProductCard.jsx
@@ -6,7 +6,10 @@ export default function ProductCard(props){
         <div className = "UserCard grid item">
             <img src={props.user.image} alt="produto" />
             <p className = "name">{props.user.name}</p>
+            <p className = "author">{props.user.author}</p>
+            <p className = "genre">{props.user.genre}</p>
             <p className = "description">{props.user.description}</p>
+            <p className = "price">{props.user.price}</p>
         </div>
     )
 }
